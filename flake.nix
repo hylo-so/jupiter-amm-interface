98c74dd52406f30efce393e3ec15abf3c3d7738a
0xPlish <plish@hylo.so>
2025-12-02 22:35:13 -0800

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Rust nightly dev environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { nixpkgs, rust-overlay, ... }:
+    let
+      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+        in {
+          default = pkgs.mkShell {
+            packages = [ pkgs.rust-bin.nightly.latest.default ];
+            buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin
+              (with pkgs.darwin.apple_sdk.frameworks; [
+                Security
+                SystemConfiguration
+              ]);
+          };
+        });
+    };
+}
